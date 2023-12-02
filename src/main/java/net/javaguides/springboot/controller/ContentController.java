59cbe00470296631d6d75bec50b4a51f7b1f9924
quanggiau <lequanggiau123@gmai.com>
2023-12-03 00:51:00 +0700

    add updt

--- src/main/java/net/javaguides/springboot/controller/ContentController.java
+++ src/main/java/net/javaguides/springboot/controller/ContentController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.javaguides.springboot.model.Content;
@@ -18,12 +20,33 @@ public class ContentController {
 	@Autowired
 	private ContentService contentService;
 
+	@GetMapping("/")
+	public String viewHomePage(Model model) {
+		return "redirect:/content/all";
+	}
+	
+
 	@GetMapping("/all")
 	public String getall(Model model) {
 		List<Content> content = contentService.getAllContents();
 		model.addAttribute("ListContent", content);
 		//return "redirect:/";
 		return "index";
+	}
+
+	@GetMapping("/showNewContentForm")
+	public String showNewContentForm(Model model) {
+		// create model attribute to bind form data
+		Content content = new Content();
+		model.addAttribute("content", content);
+		return "new_content";
+	}
+
+	@PostMapping("/saveContent")
+	public String saveContent(@ModelAttribute("content") Content content) {
+		// save employee to database
+		contentService.saveContent(content);
+		return "redirect:/content/all";
 	}
 
 }
