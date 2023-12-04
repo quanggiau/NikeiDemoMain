239f03d3379334f1f8ce312b6b14dfd5def670a9
BRYCEN-VN\lq_giau <lq_giau@brycen.com.vn>
2023-12-04 18:07:05 +0700

    begin add api chat gpt

--- src/main/java/net/javaguides/springboot/controller/ContentController.java
+++ src/main/java/net/javaguides/springboot/controller/ContentController.java
@@ -9,13 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import net.javaguides.springboot.model.Content;
-import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.ServiceMasterRepository;
 import net.javaguides.springboot.service.ContentService;
 import net.javaguides.springboot.service.ServiceMasterSRV;
@@ -43,7 +41,8 @@ public class ContentController {
 		return "new_content";
 	}
 
-	@PostMapping("/saveContent")
+	// @PostMapping("/saveContent")
+	@RequestMapping(value="/saveContent", method=RequestMethod.POST, params="saveContent")
 	public String saveContent(@ModelAttribute("content") Content content) {
 		// save content to database 
 		String ct = content.getQuestion();
@@ -61,22 +60,6 @@ public class ContentController {
 		return "redirect:/servicemaster/all";
 	}
 
-	// @GetMapping("/showFormForUpdate")
-	// public String showFormForUpdate(@RequestParam long id, Model model) {
-
-	// 	List<Content> content = contentService.getContentByIdService(id);
-	// 	model.addAttribute("content", content);
-
-	// 	//model.addAttribute("serviceMasterList", serviceMTService.getAllService());
-		
-	// 	// get employee from the service
-	// 	// Employee employee = employeeService.getEmployeeById(id);
-		
-	// 	// // set employee as a model attribute to pre-populate the form
-	// 	// model.addAttribute("employee", employee);
-	// 	return "update_content";
-	// }
-
 	@GetMapping("/showFormForUpdate")
 	public String showFormForUpdate(@RequestParam long id,Model model) {
 		// create model attribute to bind form data
@@ -86,5 +69,34 @@ public class ContentController {
 		model.addAttribute("serviceMT", repoMT.getReferenceById(id) );
 		return "update_content";
 	}
+
+		@PostMapping("/editContent")
+		public String editContent(@ModelAttribute Content content) {
+		// save content to database 
+		contentService.updateContent(content);
+		
+		return "redirect:/servicemaster/all";
+	}
+
+	
+	@RequestMapping(value="/saveContent", method=RequestMethod.POST, params="saveAndcallGPT")
+	public String saveAndcallGPT(@ModelAttribute("content") Content content) {
+		// 1. save content to database 
+		String ct = content.getQuestion();
+		String[] splCT = ct.split(",");
+		String[] splAn = content.getAnswer().split(",");
+		for(int i = 0; i<=splCT.length-1; i++)
+		{
+			Content conDB = new Content();
+			conDB.setAnswer(splAn[i]);
+			conDB.setIdService(content.getIdService());
+			conDB.setQuestion(splCT[i]);
+			contentService.saveContent(conDB);
+		}
+
+		// 2. call API
+		
+		return "redirect:/servicemaster/all";
+ 	}
 
 }
