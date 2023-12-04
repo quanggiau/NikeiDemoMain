effceb7df35af2a5a6c13f4aaf05ef5202e3507e
BRYCEN-VN\lq_giau <lq_giau@brycen.com.vn>
2023-12-04 12:45:24 +0700

    add craete content

--- src/main/java/net/javaguides/springboot/controller/ContentController.java
+++ src/main/java/net/javaguides/springboot/controller/ContentController.java
@@ -1,9 +1,9 @@
 package net.javaguides.springboot.controller;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.javaguides.springboot.model.Content;
+import net.javaguides.springboot.model.ServiceMaster;
 import net.javaguides.springboot.service.ContentService;
+import net.javaguides.springboot.service.ServiceMasterSRV;
 
 //@RestController
 @Controller
@@ -20,6 +22,9 @@ import net.javaguides.springboot.service.ContentService;
 public class ContentController {
 	@Autowired
 	private ContentService contentService;
+
+	@Autowired
+	private ServiceMasterSRV serviceMTService;
 
 	// @GetMapping("/all")
 	// public ResponseEntity getall() {
@@ -44,19 +49,20 @@ public class ContentController {
 	// 	return "index_detail";
 	// }
 
-	// @GetMapping("/showNewContentForm")
-	// public String showNewContentForm(Model model) {
-	// 	// create model attribute to bind form data
-	// 	Content content = new Content();
-	// 	model.addAttribute("content", content);
-	// 	return "new_content";
-	// }
+	@GetMapping("/showNewContentForm")
+	public String showNewContentForm(Model model) {
+		// create model attribute to bind form data
+		Content content = new Content();
+		model.addAttribute("content", content);
+		model.addAttribute("serviceMasterList", serviceMTService.getAllService());
+		return "new_content";
+	}
 
-	// @PostMapping("/saveContent")
-	// public ResponseEntity saveContent(@ModelAttribute("content") Content content) {
-	// 	// save employee to database
-	// 	contentService.saveContent(content);
-	// 	return "redirect:/content/all";
-	// }
+	@PostMapping("/saveContent")
+	public String saveContent(@ModelAttribute("content") Content content) {
+		// save employee to database
+		contentService.saveContent(content);
+		return "redirect:/servicemaster/all";
+	}
 
 }
